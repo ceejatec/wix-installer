224dcd161e9c25a8dc90c1038f6398fa3845cdca
Chris Hillery <ceej@couchbase.com>
2016-06-14 18:05:46 -0700

    Silent execution of fixpaths

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 heat dir C:\newbits -srd -suid -gg -sreg -ke -cg CouchbaseServer -dr INSTALLDIR -out Files.wxs
 candle -arch x64 *.wxs
-light -ext WixUIExtension -b C:\newbits -o Server.msi *.wixobj
+light -ext WixUIExtension -ext WixUtilExtension -b C:\newbits -o Server.msi *.wixobj
