9b23e632cc2b78846105ed5b15d5df5eb37ad808
Chris Hillery <ceej@couchbase.com>
2016-06-15 15:21:38 -0700

    Start service; modify more config file paths

--- fixpaths.cmd
+++ fixpaths.cmd
@@ -1,4 +1,22 @@
+@echo off
+setlocal
+
 set INSTALLDIR=%~1
 
-"%INSTALLDIR%\bin\fnr.exe" --cl --silent --fileMask static_config --dir "%INSTALLDIR%etc\couchbase" --useRegEx --find "C:/Jenkins/workspace/.*windows/couchbase/install/" --replace "%INSTALLDIR%\"
-"%INSTALLDIR%\bin\fnr.exe" --cl --silent --fileMask static_config --dir "%INSTALLDIR%etc\couchbase" --find "\\" --replace "/"
+call :fixfile "etc\couchbase" "static_config"
+call :fixfile "etc\couchdb" "default.ini"
+call :fixfile "etc\couchdb\default.d" "capi.ini"
+call :fixfile "etc\logrotate.d" "couchdb"
+call :fixfile "lib\couch-1.2.0a-961ad59-git\ebin" "couch.app"
+
+:: End of script
+exit /B %ERRORLEVEL%
+
+:: Function to modify a file
+:fixfile
+set DIR=%~1
+set FILENAME=%~2
+
+"%INSTALLDIR%\bin\fnr.exe" --cl --silent --fileMask "%FILENAME%" --dir "%INSTALLDIR%%DIR%" --useRegEx --find "C:/Jenkins/workspace/.*windows/couchbase/install/" --replace "%INSTALLDIR%\" 
+"%INSTALLDIR%\bin\fnr.exe" --cl --silent --fileMask "%FILENAME%" --dir "%INSTALLDIR%%DIR%" --find "\\" --replace "/" 
+exit /B 0
