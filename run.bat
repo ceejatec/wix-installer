5c2b736a981a75ba6dfde9114e7bbf39cc49d43b
Chris Hillery <ceej@couchbase.com>
2016-12-16 03:27:04 -0800

    Don't use QtExec; Error check in run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,13 @@
-copy fixpaths.cmd C:\bits\couchbase\install\bin
+copy fixpaths.cmd C:\newbits\couchbase\install\bin
 
-heat dir C:\bits\couchbase\install -srd -suid -gg -sreg -ke -cg CouchbaseServer -dr INSTALLDIR -out Files.wxs
-candle -arch x64 *.wxs
-light -ext WixUIExtension -ext WixUtilExtension -b C:\bits\couchbase\install -o Server.msi *.wixobj
+heat dir C:\newbits\couchbase\install -srd -suid -gg -sreg -ke -cg CouchbaseServer -dr INSTALLDIR -out Files.wxs || goto :error
+candle -arch x64 *.wxs || goto :error
+light -ext WixUIExtension -ext WixUtilExtension -b C:\newbits\couchbase\install -o Server.msi *.wixobj || goto :error
+
+:end
+exit /b 0
+
+:error
+@echo Previous command failed with error #%errorlevel%.
+exit /b %errorlevel%
+
