bc6f55700fb55dd162a2f9094d38343548918b89
Chris Hillery <ceej@couchbase.com>
2017-04-28 15:19:37 -0700

    Update from latest code

---
+++ create-installer.bat
@@ -0,0 +1,17 @@
+set destdir=%1
+
+copy fixpaths.cmd %destdir%\bin
+copy fnr.exe %destdir%\bin
+copy ..\CouchbaseEnterpriseLicense.rtf %destdir%
+
+heat dir %destdir% -srd -suid -gg -sreg -ke -cg CouchbaseServer -dr INSTALLDIR -out Files.wxs || goto :error
+candle -arch x64 *.wxs || goto :error
+light -ext WixUIExtension -ext WixUtilExtension -b %destdir% -dWixUILicenseRtf=..\CouchbaseEnterpriseLicense.rtf -o Server.msi *.wixobj || goto :error
+
+:end
+exit /b 0
+
+:error
+@echo Previous command failed with error #%errorlevel%.
+exit /b %errorlevel%
+
