2a014100e8481e9e2c436ada139e5032fca9c714
Chris Hillery <ceej@couchbase.com>
2017-06-05 21:36:42 -0700

    Testing upgrade; Check for UCRT

--- create-installer.bat
+++ create-installer.bat
@@ -1,12 +1,22 @@
-set destdir=%~dp0\testing
+set destdir500=%~dp0\testing-5.0.0
+set destdir501=%~dp0\testing-5.0.1
 
-copy fixpaths.cmd %destdir%\bin
-copy fnr.exe %destdir%\bin
-copy ..\CouchbaseEnterpriseLicense.rtf %destdir%
+copy fixpaths.cmd %destdir500%\bin
+copy fnr.exe %destdir500%\bin
 
-heat dir %destdir% -srd -suid -gg -sreg -ke -cg CouchbaseServer -dr INSTALLDIR -out Files.wxs || goto :error
-candle -arch x64 *.wxs || goto :error
-light -ext WixUIExtension -ext WixUtilExtension -b %destdir% -dWixUILicenseRtf=..\CouchbaseEnterpriseLicense.rtf -o Server.msi *.wixobj || goto :error
+heat dir %destdir500% -srd -suid -ag -sreg -ke -cg CouchbaseServer -dr INSTALLDIR -out Files.wxs || goto :error
+candle -arch x64 -dVersion=5.0.0 *.wxs || goto :error
+light -ext WixUIExtension -ext WixUtilExtension -b %destdir500% -dWixUILicenseRtf=..\CouchbaseEnterpriseLicense.rtf -o 500.msi *.wixobj || goto :error
+
+del *.wixobj
+copy Files.wxs Files.bak
+
+copy fixpaths.cmd %destdir501%\bin
+copy fnr.exe %destdir501%\bin
+
+heat dir %destdir501% -srd -suid -ag -sreg -ke -cg CouchbaseServer -dr INSTALLDIR -out Files.wxs || goto :error
+candle -arch x64 -dVersion=5.0.1 *.wxs || goto :error
+light -ext WixUIExtension -ext WixUtilExtension -b %destdir501% -dWixUILicenseRtf=..\CouchbaseEnterpriseLicense.rtf -o 501.msi *.wixobj || goto :error
 
 :end
 exit /b 0
